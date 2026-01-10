0a3a2e68608126d05dfda6f1af3b939c4e978219
Swathi304492 <Swathi.Palagiri@ust.trumio.ai>
2026-01-10 12:40:43 +0530

    init 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import TechlingoPage from './components/hero'
 import './App.css'
 
 function App() {
@@ -9,7 +6,6 @@ function App() {
 
   return (
     <>
-      <TechlingoPage/>
     </>
   )
 }
